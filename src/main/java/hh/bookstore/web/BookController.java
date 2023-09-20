e22cf9b68417c845a3f6dc26d95153ed4d02ecf5
Neiwyth <user.example@mailing.com>
2023-09-20 22:18:01 +0300

    Added edit book functionality

--- src/main/java/hh/bookstore/web/BookController.java
+++ src/main/java/hh/bookstore/web/BookController.java
@@ -32,7 +32,7 @@ public class BookController {
     }
 
     @PostMapping("/save")
-    public String saveNewBook(Book book) {
+    public String saveBook(Book book) {
 
         bookRepository.save(book);
         return "redirect:booklist";
@@ -42,7 +42,13 @@ public class BookController {
     public String deleteBook(@PathVariable("id") Long bookId, Model model) {
 
         bookRepository.deleteById(bookId);
-        return "redirect:/booklist";
+        return "redirect:../booklist";
     }
 
+    @GetMapping("/edit/{id}")
+    public String editBook(@PathVariable("id") Long bookId, Model model) {
+
+        model.addAttribute("book", bookRepository.findById(bookId));
+        return "editbook";
+    }
 }
